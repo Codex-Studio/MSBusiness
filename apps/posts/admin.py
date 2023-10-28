6a6c406e2541fa074e1a8c15f6991636e0513d93
didiotghima <nurlanuuulubeksultan@gmail.com>
2023-10-27 21:28:36 -0500

    end index

---
+++ apps/posts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from apps.posts.models import Main, Job, About, Directions, Advantages, Description, Application, Klient
+
+admin.site.register(Main)
+admin.site.register(Job)
+admin.site.register(About)
+admin.site.register(Directions)
+admin.site.register(Advantages)
+admin.site.register(Description)
+admin.site.register(Application)
+admin.site.register(Klient)
